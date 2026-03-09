b033b68d02834d8fbfca0ef32dc4bcfa22489584
Navdeep Singh <navdeeps@adobe.com>
2026-03-09 17:25:36 +0530

    update accordion definiton

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,21 +4,17 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    li.className = 'accordion-item';
     moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
 
-    const label = row.children[0];
-    const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
-    summary.append(...label.childNodes);
+    const [label, body] = [...li.children];
+    if (label) {
+      label.className = 'accordion-item-label';
+      label.addEventListener('click', () => li.classList.toggle('active'));
+    }
+    if (body) body.className = 'accordion-item-body';
 
-    const body = row.children[1];
-    body.className = 'accordion-item-body';
-
-    const details = document.createElement('details');
-    details.className = 'accordion-item';
-    details.append(summary, body);
-
-    li.append(details);
     ul.append(li);
   });
 
